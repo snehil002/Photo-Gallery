f408d5e5aa1cc84136e1bcf72be4dc0e18391126
Snehil <snehil@example.com>
2024-09-18 16:30:48 +0530

    remake

---
+++ myscript.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhGbS2Kptg1uYkmBcmqae-5NNltVQKKno",
+  authDomain: "photo-gallery-b9db9.firebaseapp.com",
+  projectId: "photo-gallery-b9db9",
+  storageBucket: "photo-gallery-b9db9.appspot.com",
+  messagingSenderId: "1000704217094",
+  appId: "1:1000704217094:web:3b0f1015765f53d6f87387",
+  databaseURL: "https://photo-gallery-b9db9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+const app = initializeApp(firebaseConfig);
